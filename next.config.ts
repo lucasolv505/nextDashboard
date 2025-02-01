5a57fb8df2504c704c1f6550606c5c799eaf5ac0
lucasolv505 <155476756+tridentam@users.noreply.github.com>
2025-02-01 15:17:48 -0300

    2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Ativa o modo estrito do React
+  eslint: {
+    ignoreDuringBuilds: true, // Ignora os erros de ESLint durante o build
+  },
 };
 
 export default nextConfig;
